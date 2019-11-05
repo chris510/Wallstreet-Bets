d6fb6358c1a3bffd549e7ccf5dc230f885be4a82
chris482 <christrinh5@gmail.com>
2019-11-04 17:17:29 -0800

    Add splash page css

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const video = ['./splashone.gif', './debitcard_turn.gif']
+const video = ['./splash1.gif', './debitcard_turn.mp4'];
 
 class Splash extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       video1: video[0],
-      video2: video[1]
+      video3: video[1]
     }
   }
 
@@ -16,23 +16,16 @@ class Splash extends React.Component {
       <div className="splash-main">
         <div className="splash-container-1">
           <div className="splash-part-1">
-            <div className="splash-text-1">
-              <h1>
-                Its Time
-              </h1>
+            <div className="splash-text-1 ">
+              <h1>It's Time to do</h1>
             </div>
             <div className="splash-text-2">
-              <h1>
-                To do Money
-              </h1>
+              <h1>Money</h1>
             </div>
-            <br />
             <div className="splash-text-3">
               <h2> Robinhood, a pioneer of commission-free investing,</h2>
-              <h2> gives you access to investing and more ways to make your money work harder.</h2>
-            </div>
-            <div >
-              <Link to='/signup' className="sign-up"><span>Sign Up</span></Link>
+              <h2> gives you access to investing and more</h2>
+              <h2> ways to make your money work harder.</h2>
             </div>
           </div>
             <div className="splash-vid-1">
@@ -53,7 +46,12 @@ class Splash extends React.Component {
             </div>
           </div>
           <div className="splash-vid-2">
-            <img src={this.state.video2} />
+            <video ref="vidRef"
+              src={this.state.video3}
+              width="600px" height="500px"
+              autoPlay={true} loop muted={true}
+              className="splash-video"
+            />
           </div>
         </div>
       </div>
