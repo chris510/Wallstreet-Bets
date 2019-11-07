89a307337c74e9799ccce2b4bcb42fd021093ede
chris482 <christrinh5@gmail.com>
2019-11-06 19:05:57 -0800

    precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
