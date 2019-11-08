6504e189aa0c8a4a4b670e30df47bddbdc1d417c
chris482 <christrinh5@gmail.com>
2019-11-07 17:43:58 -0800

    create stocks controller and fetch users stocks react/redux call

--- app/controllers/api/stocks_controller.rb
+++ app/controllers/api/stocks_controller.rb
@@ -1,3 +1,19 @@
 class Api::StocksController < ApplicationController
+  def index
+    @stocks = Stock.all
+  end
 
+  def show
+    @stock = selected_stock
+  end
+
+  def selected_stock
+    Stock.find(params[:symbol])
+  end
+
+  private
+
+  def stocks_params
+    params.require(:stock).permit(:name, :symbol)
+  end 
 end
