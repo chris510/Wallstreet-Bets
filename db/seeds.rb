e62796077e217181d065733c4b673d1ea11fca34
Chris Trinh <christrinh5@gmail.com>
2019-12-10 17:21:47 -0800

    Seed backend stock data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require_relative 'portfolios'
+require 'csv'
 
 ActiveRecord::Base.transaction do 
 
@@ -17,17 +18,23 @@ ActiveRecord::Base.transaction do
 
   demo_user = User.create({ username: 'Demo_User', password: 'password123'})
 
-  facebook = Stock.create({name: "Facebook", symbol: "FB"})
-  netflix = Stock.create({name: "Netflix", symbol: "NFLX"})
-  square = Stock.create({name: "Square", symbol: "SQ"})
-  guardant_health = Stock.create({name: "Guardant Health", symbol: "GH"})
-  tesla = Stock.create({name: "Tesla", symbol: "TSLA"})
+  # facebook = Stock.create({name: "Facebook", symbol: "FB"})
+  # netflix = Stock.create({name: "Netflix", symbol: "NFLX"})
+  # square = Stock.create({name: "Square", symbol: "SQ"})
+  # guardant_health = Stock.create({name: "Guardant Health", symbol: "GH"})
+  # tesla = Stock.create({name: "Tesla", symbol: "TSLA"})
   # amazon = Stock.create({name: "Amazon", symbol: "AMZN"})
   # shopify = Stock.cretate({name: "Shopify", symbol: "SHOP"})
   # google = Stock.create({name: "Google", symbol: "GOOG"})
-  zoom = Stock.create({name: "Zoom", symbol: "ZM"})
-  starbucks = Stock.create({name: "Starbucks", symbol: "SBUX"})
-  microsoft = Stock.create({name: "Microsoft", symbol: "MSFT"})
+  # zoom = Stock.create({name: "Zoom", symbol: "ZM"})
+  # starbucks = Stock.create({name: "Starbucks", symbol: "SBUX"})
+  # microsoft = Stock.create({name: "Microsoft", symbol: "MSFT"})
+
+  nasdaq_csv = File.read(Rails.root.join('db', 'nasdaq.csv'))
+  nasdaq = CSV.parse(nasdaq_csv, :headers => true, :encoding => 'ISO-8859-1')
+  nasdaq.each do |row|
+    Stock.create(symbol: row["Symbol"], name: row["Company Name"])
+  end
 
   buy_fb = Order.create({
     user_id: demo_user.id, 
