b9a8f9b85477b44f00b34ced7e27741977aa4503
Chris Trinh <christrinh5@gmail.com>
2020-01-20 19:41:20 -0800

    Refactor home page with hooks

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,55 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import NewsIndexItem from './news/news_index_item';
 import StockIndexContainer from "./stock_index_container";
 import PortfolioContainer from './portfolio_container';
 
-class Home extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Home = (props) => {
 
-  componentDidMount() {
-    this.props.fetchUserStocks().then(() => this.props.fetchStocks()); 
-    this.props.fetchNews();
-  }
+  useEffect(() => {
+    props.fetchUserStocks().then(() => this.props.fetchStocks()); 
+    props.fetchNews();
+  }, []);
 
-  render() {
-    const { news } = this.props 
-    return (
-      <div className="home">
-        <div className="home-container-1">
-            <PortfolioContainer
-              // portfolio={this.props.portfolio}
-            />
-          <div className="news-container">
-              <div className="index-title">
-                Recent News
-              </div>
-            {news.map( (singleNews, idx) => {
-              return (
-                <div className="news-index-item" key={idx}  >
-                  <NewsIndexItem
-                    key={singleNews.id}
-                    url={singleNews.url}
-                    source={singleNews.source.name}
-                    title={singleNews.title}
-                    description={singleNews.description}
-                    image={singleNews.urlToImage}
-                  />
-                </div>
-              )
-            })}
-          </div>
-        </div>
-        <div className="home-container-2">
-            <div className="stocks-dashboard-container">
-              <StockIndexContainer />
+  return (
+    <div className="home">
+      <div className="home-container-1">
+          <PortfolioContainer
+            // portfolio={this.props.portfolio}
+          />
+        <div className="news-container">
+            <div className="index-title">
+              Recent News
             </div>
+          {props.news.map( (singleNews, idx) => {
+            return (
+              <div className="news-index-item" key={idx}  >
+                <NewsIndexItem
+                  key={singleNews.id}
+                  url={singleNews.url}
+                  source={singleNews.source.name}
+                  title={singleNews.title}
+                  description={singleNews.description}
+                  image={singleNews.urlToImage}
+                />
+              </div>
+            )
+          })}
         </div>
       </div>
-    )
-  }
+      <div className="home-container-2">
+          <div className="stocks-dashboard-container">
+            <StockIndexContainer />
+          </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home;
